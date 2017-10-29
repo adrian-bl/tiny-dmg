d960b71b4b22d520e28e88cc157bfa2b7e451a4c
Adrian Ulrich <adrian@blinkenlights.ch>
2017-10-29 22:31:53 +0100

    ..and gofmt it

--- src/tiny-dmg/machine/machine.go
+++ src/tiny-dmg/machine/machine.go
@@ -42,38 +42,38 @@ func (mach *Machine) Run() {
 		op = mach.mem.GetByte(mach.cpu.Reg.PC) // raw opcode from ROM
 		dbgopcode := cpu.OpCodes[op]
 
-	if XLOG == "verbose" {
-		fmt.Printf("%04X %02X AF=%02X%02X      BC=%02X%02X       DE=%02X%02X    ", mach.cpu.Reg.PC, mach.cpu.Reg.A, mach.cpu.Reg.F, mach.cpu.Reg.B, mach.cpu.Reg.C, mach.cpu.Reg.D, mach.cpu.Reg.E)
-		fmt.Printf("HL=%02X%02X   SP=%04X [", mach.cpu.Reg.H, mach.cpu.Reg.L, mach.cpu.Reg.SP)
-		if mach.cpu.Reg.F&cpu.FlagZ != 0 {
-			fmt.Printf("Z")
-		} else {
-			fmt.Printf("-")
-		}
-		if mach.cpu.Reg.F&cpu.FlagN != 0 {
-			fmt.Printf("N")
-		} else {
-			fmt.Printf("-")
-		}
-		if mach.cpu.Reg.F&cpu.FlagH != 0 {
-			fmt.Printf("H")
-		} else {
-			fmt.Printf("-")
-		}
-		if mach.cpu.Reg.F&cpu.FlagC != 0 {
-			fmt.Printf("C")
-		} else {
-			fmt.Printf("-")
+		if XLOG == "verbose" {
+			fmt.Printf("%04X %02X AF=%02X%02X      BC=%02X%02X       DE=%02X%02X    ", mach.cpu.Reg.PC, mach.cpu.Reg.A, mach.cpu.Reg.F, mach.cpu.Reg.B, mach.cpu.Reg.C, mach.cpu.Reg.D, mach.cpu.Reg.E)
+			fmt.Printf("HL=%02X%02X   SP=%04X [", mach.cpu.Reg.H, mach.cpu.Reg.L, mach.cpu.Reg.SP)
+			if mach.cpu.Reg.F&cpu.FlagZ != 0 {
+				fmt.Printf("Z")
+			} else {
+				fmt.Printf("-")
+			}
+			if mach.cpu.Reg.F&cpu.FlagN != 0 {
+				fmt.Printf("N")
+			} else {
+				fmt.Printf("-")
+			}
+			if mach.cpu.Reg.F&cpu.FlagH != 0 {
+				fmt.Printf("H")
+			} else {
+				fmt.Printf("-")
+			}
+			if mach.cpu.Reg.F&cpu.FlagC != 0 {
+				fmt.Printf("C")
+			} else {
+				fmt.Printf("-")
+			}
+
+			fmt.Printf("] c=%d ## %d, c=%d, LY(FF44) = %X, >> FIXME: STAT = %X (%X), LCDC=%02X, op=%s\n", dbgopcode.ClockCycles, i, mach.cpu.ClockCycles, mach.mem.GetByte(0xFF44), mach.mem.GetByte(0xFF41), mach.mem.GetByte(0xFF41)&0x3, mach.mem.GetByte(0xFF40), dbgopcode.Name)
 		}
 
-		fmt.Printf("] c=%d ## %d, c=%d, LY(FF44) = %X, >> FIXME: STAT = %X (%X), LCDC=%02X, op=%s\n", dbgopcode.ClockCycles, i, mach.cpu.ClockCycles, mach.mem.GetByte(0xFF44), mach.mem.GetByte(0xFF41), mach.mem.GetByte(0xFF41)&0x3, mach.mem.GetByte(0xFF40), dbgopcode.Name)
-	}
-
-	if XLOG == "mgba" {
-		fmt.Printf("A: %02X F: %02X B: %02X C: %02X D: %02X E: %02X H: %02X L: %02X SP: %04X PC: %04X |\n",
-			mach.cpu.Reg.A, mach.cpu.Reg.F, mach.cpu.Reg.B, mach.cpu.Reg.C,
-			mach.cpu.Reg.D, mach.cpu.Reg.E, mach.cpu.Reg.H, mach.cpu.Reg.L, mach.cpu.Reg.SP, mach.cpu.Reg.PC)
-	}
+		if XLOG == "mgba" {
+			fmt.Printf("A: %02X F: %02X B: %02X C: %02X D: %02X E: %02X H: %02X L: %02X SP: %04X PC: %04X |\n",
+				mach.cpu.Reg.A, mach.cpu.Reg.F, mach.cpu.Reg.B, mach.cpu.Reg.C,
+				mach.cpu.Reg.D, mach.cpu.Reg.E, mach.cpu.Reg.H, mach.cpu.Reg.L, mach.cpu.Reg.SP, mach.cpu.Reg.PC)
+		}
 
 		i++
 
